9fd33f391f2e067f57d24a3f135c46dcfd86393e
unknown <jamesugbanu@gmail.com>
2018-09-26 12:11:18 +0100

    ch-persisting-data-to-database

--- api/server.js
+++ api/server.js
@@ -6,17 +6,20 @@ const app = express()
 import bodyParser from "body-parser";
 app.use(bodyParser.urlencoded({extended: true}));
 import OrderWithJsObject from './src/usingJSObject/controllers/Orders';
+import OrderWithDB from './src/usingDB/controller/Order';
+import UserWithDb from './src/usingDB/controller/Users';
 
-const order = process.env.TYPE === 'db' ? OrderWithDB : OrderWithJsObject;
+//const order = process.env.TYPE === 'db' ? OrderWithDB : OrderWithJsObject;
 
 app.use(express.json())
 
-app.post('/api/v1/orders', order.createOrder);
-app.get('/api/v1/orders', order.getAllOrder);
-app.get('/api/v1/orders/:id', order.getOrderById);
-app.put('/api/v1/orders/:id', order.updateOrderStatus);
-app.delete('/api/v1/orders/:id', order.deleteOrder);
-
+// app.post('/api/v1/orders', order.createOrder);
+// app.get('/api/v1/orders', order.getAllOrder);
+// app.get('/api/v1/orders/:id', order.getOrderById);
+// app.put('/api/v1/orders/:id', order.updateOrderStatus);
+// app.delete('/api/v1/orders/:id', order.deleteOrder);
+app.post('/api/v1/users/register', UserWithDb.createUser);
+app.post('/api/v1/users/authenticate', UserWithDb.authenticateUser);
 
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'Wow! Nice! Your first working endpoint'});
--- api/src/UsingDB/controller/Helper.js
+++ api/src/UsingDB/controller/Helper.js
@@ -1,1 +1,29 @@
-Helper.js
+import bcrypt from 'bcrypt-nodejs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+const Helper = {
+
+  hashPassword(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+  },
+
+  comparePassword(hashPassword, password) {
+    return bcrypt.compareSync(password, hashPassword);
+  },
+
+  isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  },
+
+  generateToken(id) {
+    const token = jwt.sign({
+      userId: id
+    },
+      process.env.SECRET, { expiresIn: '7d' }
+    );
+    return token;
+  }
+}
+
+export default Helper;

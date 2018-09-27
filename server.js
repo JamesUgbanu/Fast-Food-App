7c4a7cdef9ec8ae786764bdce31cc6687a2f4a36
unknown <jamesugbanu@gmail.com>
2018-09-27 23:00:55 +0100

    test

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(3000)
-console.log('app running on port ', 3000);
+app.listen(process.env.PORT || 5000)
+console.log('app running on port ', 5000);

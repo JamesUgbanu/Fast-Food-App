0aeb9e79b56591d2267b688a809f850376d454a9
unknown <jamesugbanu@gmail.com>
2018-10-16 16:55:09 +0100

    fix port issue

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.get('/', (req, res) => {
 app.get("*", (req,res) => {
   res.status(404).send({success: "false", message: "EndPoint does not exist"});
 });
-const PORT = app.listen(process.env.PORT || 5000)
-console.log('app running on port 5000');
+const PORTS = app.listen(process.env.PORT)
+console.log(`app running on port ${process.env.PORT}`);
